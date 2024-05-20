1750dd7b1d95034bd550f5b997c210feef910d7f
Luissosa2626 <90869025+Luissosa2626@users.noreply.github.com>
2024-05-19 21:06:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ export default function Home() {
   return <Layout>
     <div className="text-blue-900 flex justify-between">
     Saludos, Mi nombre es Luis Canot. Aqui les comparto el proyecto que me han propuesto en la entrevista,
-    el cual he subido a mi repositorio de Github para que puedan visualizar el codigo y descargarlo tambien,
+    el cual he subido a mi repositorio de Github para que puedan visualizar el codigo y tambien descargarlo,
     mientras que el despliegue de la aplicacion fue subido a Vercel para visualizar su funcionamiento.
-    En la documentacion del proyecto estaran especificados mas detalles detalles de la misma.
+    En la documentacion del proyecto estaran especificados los detalles de la misma.
     Gracias de antemano por la oportunidad de participar en su proceso de seleccion.
     </div>
     </Layout>
